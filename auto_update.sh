9c942e9f50931fa65ef1928ecdc764d5671e1b02
yaotian <yaotian@gmail.com>
2014-03-02 11:31:23 +0800

    Update auto_update.sh

--- auto_update.sh
+++ auto_update.sh
@@ -2,12 +2,5 @@
 
 cd /opt/applications/go_markdown_server/
 git pull origin|grep "Already up-to-date"
-result=$?
-echo $result
-
-if [ $result = 0 ]
-then
-    echo "start to gor compile"
-    cd zhengyaotian.com
-    gor compile
-fi
+cd zhengyaotian.com
+gor compile
